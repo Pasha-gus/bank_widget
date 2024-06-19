7f42d4a0327c5d0ad04bb207517de74179e8574f
Pavel Polezhaev <pavelpoleczaev@gmail.com>
2024-06-19 21:28:43 +0300

    src/widget.py

--- src/widget.py
+++ src/widget.py
@@ -1,10 +1,19 @@
-import masks
+from src.masks import get_mask_account, get_mask_card_number
 def mask_account_card(card_name_mask: str) -> str:
-    name_mask = card_name_mask.split()
+    """Функция возвращающая названия и маску карты"""
+    name_mask = card_name_mask.split(" ")
     if len(name_mask[1]) == 16:
-        return f"{name_mask[0]} {masks.get_mask_card_number(name_mask[1])}"
+        return f"{name_mask[0]} {get_mask_card_number(name_mask[1])}"
     elif len(name_mask[1]) == 20:
-        return f"{name_mask[0]} {masks.get_mask_account(name_mask[1])}"
+        return f"{name_mask[0]} {get_mask_account(name_mask[1])}"
     else:
         return "Неверный формат"
+
+
+def get_data(data_time: str) -> str:
+    parts = data_time.split("T")
+    data = parts[0]
+    format_data = data[8:] + "." + data[5:7] + "." + data[:4]
+    return format_data
+
 

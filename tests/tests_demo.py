d5a9532514daef2fb4042f46c2e47a02d71815a4
Pavel Polezhaev <pavelpoleczaev@gmail.com>
2024-06-24 23:38:09 +0300

    update tasts

--- tests/tests_demo.py
+++ tests/tests_demo.py
@@ -1,5 +1,6 @@
 from src.masks import get_mask_account, get_mask_card_number
 from src.widget import get_data, mask_account_card
+from src.processing import filter_by_state, sort_by_date
 
 number_card = 7000792289606361
 number_check = 73654108430135874305
@@ -11,3 +12,25 @@ print(f"Маска номера счета: {get_mask_account(number_check)}")
 print(f"Маска номера карты: {mask_account_card(name_number_card)}")
 print(f"Маска номера счета: {mask_account_card(name_number_check)}")
 print(f"Дата: {get_data(data)}")
+
+sorted_list_desc = filter_by_state(
+    [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+)
+
+print(sorted_list_desc)
+
+sorted_list_desc = sort_by_date(
+    [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
+)
+
+print(sorted_list_desc)

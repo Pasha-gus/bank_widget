ce3eeceaaed01f9f9f8bb753a9c0e2891fd2437a
Pavel Polezhaev <pavelpoleczaev@gmail.com>
2024-07-23 13:18:00 +0300

    add xlsx and csv files

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,12 @@
+import csv
 import json
 import logging
 import os.path
 
+import pandas as pd
+
 logger = logging.getLogger("utils")
-logger.setLevel(logging.CRITICAL)
+logger.setLevel(logging.INFO)
 file_handler = logging.FileHandler("..\\logs\\utils.log", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
@@ -27,3 +30,42 @@ def transaction_data(path_file: str) -> list:
         logger.critical(f"Файл {path_file} не содержит список")
         return []
     return data_file
+
+
+def transaction_data_csv(path_file: str) -> list:
+    """Принимает на вход путь до CSV-файла и возвращает список словарей с данными о финансовых транзакциях."""
+    data_file = []
+    try:
+        logger.info("Считываем содержимое файла")
+        with open(path_file, "r", encoding="utf-8") as file:
+            reader = csv.DictReader(file, delimiter=";")
+            for row in reader:
+                data_file.append(row)
+        return data_file
+
+    except FileNotFoundError:
+        logger.error(f"Ошибка: файл {path_file} не существует")
+        return []
+    except csv.Error as csv_err:
+        logger.error(f"Ошибка при считывании файла CSV: {csv_err}")
+        return []
+    except Exception as ex:
+        logger.error(f"Ошибка при считывании файла: {ex}")
+        return []
+
+
+def transaction_data_excel(path_file: str) -> list:
+    try:
+        logger.info(f"Считываем содержимое файла {path_file}")
+        excel_data = pd.read_excel(path_file)
+        transaction_list = excel_data.to_dict(orient="records")
+        return transaction_list
+    except FileNotFoundError:
+        logger.error(f"Ошибка: Файл {path_file} не найден")
+        return []
+    except ValueError as ex:
+        logger.error(f"Ошибка: {ex} неправильный формат файла")
+        return []
+    except Exception as ex:
+        logger.error(f"Ошибка: {ex}")
+        return []
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,6 @@
-from unittest.mock import mock_open, patch
+from unittest.mock import Mock, mock_open, patch
 
-from src.utils import transaction_data
+from src.utils import transaction_data, transaction_data_csv, transaction_data_excel
 
 
 def test_file_not_exists():
@@ -38,3 +38,62 @@ def test_empty_list(mock_open, mock_exists):
     mock_exists.return_value = True
     mock_open.return_value.read.return_value = "[]"
     assert transaction_data("empty_list.json") == []
+
+
+def test_file_csv_not_exist():
+    result = transaction_data_csv("fake_path")
+    assert result == []
+
+
+def test_transaction_data_csv_success():
+    mock_file_content = "id;state;date;amount;currency_name;currency_code;from;to;description\n650703;EXECUTED;2023-09-05T11:30:32Z;16210;Sol;PEN;Счет 58803664561298323391;Счет 39745660563456619397;Перевод организации\n"
+
+    with patch("builtins.open", mock_open(read_data=mock_file_content)):
+        result = transaction_data_csv("mocked_path.csv")
+
+    expected_result = [
+        {
+            "id": "650703",
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": "16210",
+            "currency_name": "Sol",
+            "currency_code": "PEN",
+            "from": "Счет 58803664561298323391",
+            "to": "Счет 39745660563456619397",
+            "description": "Перевод организации",
+        }
+    ]
+
+    assert result == expected_result
+
+
+def test_transaction_data_excel_success():
+    mock_data = Mock()
+    mock_data.to_dict.return_value = [{"column1": "value1", "column2": "value2"}]
+
+    with patch("pandas.read_excel", return_value=mock_data):
+        result = transaction_data_excel("dummy_path.xlsx")
+
+        assert result == [{"column1": "value1", "column2": "value2"}]
+
+
+def test_transaction_data_excel_file_not_found():
+    with patch("pandas.read_excel", side_effect=FileNotFoundError):
+        result = transaction_data_excel("dummy_path.xlsx")
+
+        assert result == []
+
+
+def test_transaction_data_excel_value_error():
+    with patch("pandas.read_excel", side_effect=ValueError("Invalid format")):
+        result = transaction_data_excel("dummy_path.xlsx")
+
+        assert result == []
+
+
+def test_transaction_data_excel_other_exception():
+    with patch("pandas.read_excel", side_effect=Exception("Some other error")):
+        result = transaction_data_excel("dummy_path.xlsx")
+
+        assert result == []
